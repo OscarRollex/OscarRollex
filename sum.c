3543ff5d93ce8675672887b67e4247508f379654
OscarRollex <102280841+OscarRollex@users.noreply.github.com>
2022-03-31 09:37:57 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+//program to find sum of 5 numbers
+#include<stdio.h>
+int main()
+{
+    int a=4;
+    int b=3;
+    int c=6;
+    int d=5;
+    int e=8;
+    int sum;
+    sum=a+b+c+d+e;
+    printf("The sum of 5 numbers is: %d", sum);
+    return 0;
+}
