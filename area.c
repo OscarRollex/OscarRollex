c7cd7e68efd36216144d6a8a3f0bb25afadb31d0
OscarRollex <102280841+OscarRollex@users.noreply.github.com>
2022-03-31 09:38:23 +0300

    Add files via upload

---
+++ area.c
@@ -0,0 +1,10 @@
+//program to print area of circle
+#include<stdio.h>
+int main()
+{
+    float r=21;
+    float area;
+    area=3.142*r*r;
+    printf("The area of circle is %f", area);
+    return 0;
+}
