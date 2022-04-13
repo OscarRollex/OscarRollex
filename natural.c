65906774c6f66efae588d7f13b6ccf2da7528d68
OscarRollex <102280841+OscarRollex@users.noreply.github.com>
2022-04-13 16:47:09 +0300

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,11 @@
+//program to display the first 50 natural numbers in reverse order
+#include<stdio.h>
+int main()
+{
+    int num;
+    for(num=50; num>=1; num--)
+    {
+        printf("%d", num);
+    }
+    return 0;
+}
