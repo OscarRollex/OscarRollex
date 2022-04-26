c54ddc4371b4d61fa16dc7c2d6042f76950ef4c6
OscarRollex <102280841+OscarRollex@users.noreply.github.com>
2022-04-26 12:03:49 +0300

    Add files via upload

---
+++ arithmetic_operators.c
@@ -0,0 +1,17 @@
+//program to determine use of arithmetic operators
+#include<stdio.h>
+int main()
+{
+    int x=20, y=4, z;
+    z=x+y;
+    printf("x+y=%d \n", z);
+    z=x-y;
+    printf("x-y=%d \n", z);
+    z=x*y;
+    printf("x*y=%d \n", z);
+    z=x/y;
+    printf("x/y=%d \n", z);
+    z=x%y;
+    printf("x%y=%d \n", z);
+    return 0;
+}
