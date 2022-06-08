f983043d9182c105933b45dc6a3508a4e0257ba9
OscarRollex <102280841+OscarRollex@users.noreply.github.com>
2022-06-08 16:24:00 +0300

    Add files via upload

---
+++ array.c
@@ -0,0 +1,51 @@
+//program to print numbers in reverse order assuming they are stored in an array
+#include<stdio.h>
+int main()
+{
+    int num[9]={34,56,54,32,67,89,90,32,21},a, b, c, d, e, f, g, h, i;
+    for(a=34;a>=0;a--)
+    {
+        printf("%d\t", a);
+    }
+    printf("%d\n");
+    for(b=56;b>=0;b--)
+    {
+        printf("%d\t", b);
+    }
+    printf("%d\n");
+    for(c=54;c>=0;c--)
+    {
+        printf("%d\t", c);
+    }
+    printf("%d\n");
+    for(d=32;d>=0;d--)
+    {
+        printf("%d\t", d);
+    }
+    printf("%d\n");
+    for(e=67;e>=0;e--)
+    {
+        printf("%d\t", e);
+    }
+    printf("%d\n");
+    for(f=89;f>=0;f--)
+    {
+        printf("%d\t", f);
+    }
+    printf("%d\n");
+    for(g=90;g>=0;g--)
+    {
+        printf("%d\t", g);
+    }
+    printf("%d\n");
+    for(h=32;h>=0;h--)
+    {
+        printf("%d\t", h);
+    }
+    printf("%d\n");
+    for(i=21;i>=0;i--)
+    {
+        printf("%d\t", i);
+    }
+    return 0;
+}
